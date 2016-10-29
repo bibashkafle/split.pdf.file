705a6f9661ef85e350c59dec448422801718f4d0
BbashKafle <blog.bibash@gmail.com>
2016-10-29 21:56:56 +0545

    Added Project

---
+++ Split_Pdf/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Split_Pdf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnChooseFile = new System.Windows.Forms.Button();
+            this.selectedFile = new System.Windows.Forms.TextBox();
+            this.totalPage = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.skip = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtSaveLocation = new System.Windows.Forms.TextBox();
+            this.btnSaveLocation = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnChooseFile
+            // 
+            this.btnChooseFile.Location = new System.Drawing.Point(206, 8);
+            this.btnChooseFile.Name = "btnChooseFile";
+            this.btnChooseFile.Size = new System.Drawing.Size(66, 24);
+            this.btnChooseFile.TabIndex = 0;
+            this.btnChooseFile.Text = "Choose";
+            this.btnChooseFile.UseVisualStyleBackColor = true;
+            this.btnChooseFile.Click += new System.EventHandler(this.btnChooseFile_Click);
+            // 
+            // selectedFile
+            // 
+            this.selectedFile.AccessibleRole = System.Windows.Forms.AccessibleRole.RowHeader;
+            this.selectedFile.Location = new System.Drawing.Point(12, 12);
+            this.selectedFile.Name = "selectedFile";
+            this.selectedFile.ReadOnly = true;
+            this.selectedFile.Size = new System.Drawing.Size(188, 20);
+            this.selectedFile.TabIndex = 1;
+            // 
+            // totalPage
+            // 
+            this.totalPage.Location = new System.Drawing.Point(31, 91);
+            this.totalPage.Name = "totalPage";
+            this.totalPage.ReadOnly = true;
+            this.totalPage.Size = new System.Drawing.Size(100, 20);
+            this.totalPage.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Total Page";
+            // 
+            // skip
+            // 
+            this.skip.Location = new System.Drawing.Point(147, 91);
+            this.skip.Name = "skip";
+            this.skip.Size = new System.Drawing.Size(100, 20);
+            this.skip.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(160, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Skip";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Enabled = false;
+            this.btnStart.Location = new System.Drawing.Point(92, 118);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(66, 24);
+            this.btnStart.TabIndex = 10;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtSaveLocation
+            // 
+            this.txtSaveLocation.AccessibleRole = System.Windows.Forms.AccessibleRole.RowHeader;
+            this.txtSaveLocation.Location = new System.Drawing.Point(12, 42);
+            this.txtSaveLocation.Name = "txtSaveLocation";
+            this.txtSaveLocation.ReadOnly = true;
+            this.txtSaveLocation.Size = new System.Drawing.Size(188, 20);
+            this.txtSaveLocation.TabIndex = 12;
+            // 
+            // btnSaveLocation
+            // 
+            this.btnSaveLocation.Location = new System.Drawing.Point(206, 38);
+            this.btnSaveLocation.Name = "btnSaveLocation";
+            this.btnSaveLocation.Size = new System.Drawing.Size(66, 24);
+            this.btnSaveLocation.TabIndex = 11;
+            this.btnSaveLocation.Text = "Save";
+            this.btnSaveLocation.UseVisualStyleBackColor = true;
+            this.btnSaveLocation.Click += new System.EventHandler(this.btnSaveLocation_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 154);
+            this.Controls.Add(this.txtSaveLocation);
+            this.Controls.Add(this.btnSaveLocation);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.skip);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.totalPage);
+            this.Controls.Add(this.selectedFile);
+            this.Controls.Add(this.btnChooseFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChooseFile;
+        private System.Windows.Forms.TextBox selectedFile;
+        private System.Windows.Forms.TextBox totalPage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox skip;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtSaveLocation;
+        private System.Windows.Forms.Button btnSaveLocation;
+    }
+}
+
---
+++ Split_Pdf/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Path = System.IO.Path;
+
+namespace Split_Pdf
+{
+    public partial class Form1 : Form
+    {
+        public Form1(){
+            InitializeComponent();
+        }
+
+        private void btnChooseFile_Click(object sender, EventArgs e){
+            var ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+            selectedFile.Text = ofd.FileName;
+        }
+
+        private void btnSaveLocation_Click(object sender, EventArgs e)
+        {
+            var ofd = new FolderBrowserDialog();
+            ofd.ShowDialog();
+            txtSaveLocation.Text = ofd.SelectedPath;
+            if ((!string.IsNullOrWhiteSpace(selectedFile.Text)) && (!string.IsNullOrWhiteSpace(txtSaveLocation.Text)))
+                btnStart.Enabled = true;
+        }
+
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            StartSplit();
+        }
+
+        private void StartSplit()
+        {
+			try
+			{
+				var extension = Path.GetExtension(selectedFile.Text);
+				if (extension != null && extension.ToLower() != ".pdf"){
+					MessageBox.Show("INVALID DOCUMENT FORMAT " + Environment.NewLine + "PLEASE CHOOSE PDF FORMAT ONLY", "Invalid", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+					return;
+				}
+
+				if (string.IsNullOrWhiteSpace(txtSaveLocation.Text)){
+					MessageBox.Show("Please choose save location");
+					return;
+				}
+
+				var frontFolderName = "Part_1";
+				var backFolderName = "Part_2";
+
+				if(!Directory.Exists(txtSaveLocation.Text+@"/"+frontFolderName))
+					Directory.CreateDirectory(txtSaveLocation.Text+@"/"+frontFolderName);
+				
+				if(!Directory.Exists(txtSaveLocation.Text+@"/"+backFolderName))
+					Directory.CreateDirectory(txtSaveLocation.Text+@"/"+backFolderName);
+				
+				using (PdfReader reader = new PdfReader(selectedFile.Text))
+				{
+					PdfReader.unethicalreading = true;
+					var frontFiles = new string[0];
+					var backFiles = new string[0];
+					totalPage.Text = reader.NumberOfPages.ToString();
+					var start = string.IsNullOrWhiteSpace(skip.Text) ? 1 : int.Parse(skip.Text);
+
+					for (var i = start; i <= reader.NumberOfPages; i++)
+					{
+						var isFront = ((i % 2) != 0);
+						var fileName = "";
+						if(isFront)
+							fileName = txtSaveLocation.Text+@"/"+frontFolderName+@"/" + i + ".pdf";
+						else
+							fileName = txtSaveLocation.Text+@"/"+backFolderName+@"/" + i + ".pdf";
+						
+						 //fileName = txtSaveLocation.Text + @"/" + isFront + "_" + i + ".pdf";
+						ExtractPages(reader, fileName, i);
+
+						if (isFront){
+							Array.Resize(ref frontFiles, frontFiles.Length + 1);
+							frontFiles[frontFiles.Length - 1] = fileName;
+						}
+						else{
+							Array.Resize(ref backFiles, backFiles.Length + 1);
+							backFiles[backFiles.Length - 1] = fileName;
+						}
+					}
+					CombineMultiplePDFs(frontFiles,txtSaveLocation.Text,true);
+					Array.Reverse (backFiles);
+					CombineMultiplePDFs(backFiles, txtSaveLocation.Text, false);
+					MessageBox.Show("Operation Completed");
+				}
+			}
+			catch(Exception ex) {
+				MessageBox.Show(ex.Message);
+			}   
+        }
+
+        private static void ExtractPages(PdfReader inputPdf, string outputFile, int pageNo)
+        {
+			try
+			{
+				// load the input document
+				var inputDoc = new Document(inputPdf.GetPageSizeWithRotation(1));
+
+				// create the filestream
+				using (var fs = new FileStream(outputFile, FileMode.Create))
+				{
+					// create the output writer 
+					PdfWriter outputWriter = PdfWriter.GetInstance(inputDoc, fs);
+					inputDoc.Open();
+					PdfContentByte cb1 = outputWriter.DirectContent;
+
+					// copy pages from input to output document
+
+					inputDoc.SetPageSize(inputPdf.GetPageSizeWithRotation(pageNo));
+					inputDoc.NewPage();
+
+					PdfImportedPage page = outputWriter.GetImportedPage(inputPdf, pageNo);
+					int rotation = inputPdf.GetPageRotation(pageNo);
+
+					if (rotation == 90 || rotation == 270)
+					{
+						cb1.AddTemplate(page, 0, -1f, 1f, 0, 0,
+							inputPdf.GetPageSizeWithRotation(pageNo).Height);
+					}
+					else
+					{
+						cb1.AddTemplate(page, 1f, 0, 0, 1f, 0, 0);
+					}
+					inputDoc.Close();
+				}	
+			}
+			catch(Exception ex){
+				MessageBox.Show(ex.Message);
+			}          
+        }
+
+        private void CombineMultiplePDFs(string[] files, string path, bool isFront)
+        {
+            var part1FileName = path + @"/part_1.pdf";
+            var part2FileName = path + @"/part_2.pdf";
+            var outFile = (isFront ? part1FileName : part2FileName);
+
+            // step 1: creation of a document-object
+            Document document = new Document();
+            document.NewPage();
+            // step 2: we create a writer that listens to the document
+            PdfCopy writer = new PdfCopy(document, new FileStream(outFile, FileMode.Create));
+            if (writer == null)
+                return;
+
+            // step 3: we open the document
+            document.Open();
+         
+            foreach (string fileName in files)
+            {
+                if (!string.IsNullOrWhiteSpace(fileName))
+                {
+                    // we create a reader for a certain document
+                    var reader = new PdfReader(fileName);
+                    reader.ConsolidateNamedDestinations();
+
+                    // step 4: we add content
+                    for (int i = 1; i <= reader.NumberOfPages; i++){
+                        PdfImportedPage page = writer.GetImportedPage(reader, i);
+                        writer.AddPage(page);
+                    }
+                    reader.Close();
+                }
+            }
+
+            writer.Close();
+            document.Close();
+        }
+    }
+}
